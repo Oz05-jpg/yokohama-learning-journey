18af46563379c89ecc691f193daeba01262c4374
warut mumchanda <warutmcd05@gmail.com>
2026-05-16 14:08:18 +0900

    Initial commit - Day 1 Hello World

---
+++ OTCalculator/OTCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OTCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Day 1 - " + DateTime.Now.ToString("dd/MM/yyyy"));
+        }
+    }
+}
