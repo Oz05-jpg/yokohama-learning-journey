2fb44d06f4dd5cb5fa87eb6765cb710d81235d41
warut mumchanda <warutmcd05@gmail.com>
2026-05-17 20:41:05 +0900

    Week1 - OT Calculator App completed

--- OTCalculator/OTCalculator/Program.cs
+++ OTCalculator/OTCalculator/Program.cs
@@ -1,11 +1,61 @@
-﻿namespace OTCalculator
+﻿//เก็บขัอมูลพนักงาน
+List<string> employeename = new List<string>();
+List<int> hoursWorked = new List<int>();
+List<double> hourlyRate = new List<double>();
+
+//ตัวแปรสำหรับควบคุมการเพิ่มพนักงาน
+string choice = "y";
+
+//วนลูปเพื่อรับข้อมูลพนักงานจนกว่าผู้ใช้จะเลือกไม่เพิ่มพนักงานอีก
+while (choice == "y")
 {
-    internal class Program
+    //รับข้อมูลพนักงาน
+    Console.WriteLine($"ชื่อพนักงาน:");
+    string name = Console.ReadLine();
+
+    Console.WriteLine($"ชั่วโมงที่ทำงาน:");
+    int hours = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"ราคาต่อชั่วโมง:");
+    double rate = double.Parse(Console.ReadLine());//parse คือการแปลงข้อมูลจาก string เป็นชนิดข้อมูลที่ต้องการ
+
+    //เพิ่มข้อมูลพนักงานลงใน list
+    employeename.Add(name);
+    hoursWorked.Add(hours);
+    hourlyRate.Add(rate);
+
+    //ถามผู้ใช้ว่าต้องการเพิ่มพนักงานคนต่อไปไหม
+    Console.WriteLine("เพิ่มพนักงานคนต่อไปไหม? (y/n)");
+    choice = Console.ReadLine();
+}
+
+
+//คำนวณค่า OT
+Console.WriteLine("\n ======= สรุปรายการ =======");
+
+// วนลูปเพื่อคำนวณค่า OT สำหรับแต่ละพนักงาน
+for (int i = 0; i < employeename.Count; i++)
+{
+    int normalHours = 40;
+    double actualHours = Math.Min(hoursWorked[i], normalHours);// ชั่วโมงที่ทำงานจริงที่ไม่เกิน 40 ชั่วโมง
+    double basepay = actualHours * hourlyRate[i];// เงินเดือนปกติสำหรับ 40 ชั่วโมงแรก
+    double otPay = 0;
+
+    // คำนวณค่า OT ถ้าชั่วโมงที่ทำงานเกิน 40 ชั่วโมง
+    if (hoursWorked[i] > normalHours)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Day 1 - " + DateTime.Now.ToString("dd/MM/yyyy"));
-        }
+        int otHours = hoursWorked[i] - normalHours;
+        otPay = otHours * hourlyRate[i] * 1.5; // OT คิดเป็น 1.5 เท่าของค่าจ้างปกติ
     }
+
+    // แสดงผลลัพธ์
+    double totalPay = basepay + otPay;
+
+    Console.WriteLine($"พนักงาน: {employeename[i]}");
+    Console.WriteLine($"ชั่วโมงที่ทำงาน: {hoursWorked[i]} ชม.");
+    Console.WriteLine($"เงินเดือนปกติ: {basepay:N2}บาท");
+    Console.WriteLine($"เงิน OT: {otPay:N2}บาท");
+    Console.WriteLine($"เงินเดือนรวม: {totalPay:N2}บาท");
+    Console.WriteLine("-----------------------------");
+
 }
--- OTCalculator/OTCalculator/obj/Debug/net10.0/OTCalculator.AssemblyInfo.cs
+++ OTCalculator/OTCalculator/obj/Debug/net10.0/OTCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a0a2824f90ccbab78aa3257bb213ad230f31ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18af46563379c89ecc691f193daeba01262c4374")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
